bae00f106783b0a593cb49bb0a94c8160c6aafd9
st10443220 <st10443220@vcconnect.edu.za>
2024-08-18 23:13:58 +0200

    First Commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const socket = io("http://localhost:3500");
+
+const getImageButton = document.querySelector(".getImage");
+const urlInput = document.querySelector(".url");
+const imgDisplay = document.querySelector(".image")
+const loadingBar = document.getElementById("loadingBar");
+
+function showLoading() {
+    loadingBar.style.display = 'block';
+}
+
+function hideLoading() {
+    loadingBar.style.display = 'none';
+}
+
+function sendUrl(e) {
+    e.preventDefault();
+
+    imgDisplay.setAttribute("src", "")
+
+    const urlValue = urlInput.value;
+
+    if (urlValue) {
+        showLoading()
+        socket.emit("message", { url: urlValue });
+        console.log(`Sent URL: ${urlValue}`);
+
+        // Listen for the server response
+        socket.on("message", ({ imageSource }) => {
+            hideLoading()
+            if (imageSource) {
+                imgDisplay.setAttribute("src", imageSource)
+                console.log(`Received image source from server: ${imageSource}`);
+            } else {
+                console.log("No image source found or an error occurred.");
+                imgDisplay.setAttribute("alt", "No image source found or an error occurred.")
+            }
+        });
+
+        urlInput.value = "";
+    } else {
+        console.log("URL input is empty. Please enter a URL.");
+    }
+}
+
+getImageButton.addEventListener("click", sendUrl);
